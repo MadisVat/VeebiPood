b921728a3eca4149cf7b73f4e69c08d9623c325a
tonisalvet <tonis.alvet@gmail.com>
2020-09-23 09:37:58 +0300

    eemalda stuff

--- src/main/java/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-import com.VeebiPood.service.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
